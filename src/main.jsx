2c6303ccc17ff6ca3403784da8463c50a02af513
Abhay Panchal <117556833+9xVibee@users.noreply.github.com>
2023-10-24 19:13:46 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import './App.css';
 import './index.css'
 
+// deploy on -> https://6537c96ff6d3fe09c23d4e25--calm-melba-4f0bd9.netlify.app/
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
